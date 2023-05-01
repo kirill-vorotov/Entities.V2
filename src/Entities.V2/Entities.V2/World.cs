93f68c5bef974b6d38d34697ad7fbb647d69ffa6
Kirill Vorotov <k.vorotov@gmail.com>
2023-05-02 00:05:40 +0100

    Removed unused field.

--- src/Entities.V2/Entities.V2/World.cs
+++ src/Entities.V2/Entities.V2/World.cs
@@ -5,14 +5,12 @@ namespace kv.Entities.V2
     public sealed partial class World
     {
         internal readonly TypeMap<TypeInfo> ComponentTypes;
-        internal readonly IComponentArrayPool ComponentArrayPool;
 
         internal EntityRepo EntityRepo;
         
         public World(TypeMap<TypeInfo> componentTypes, IComponentArrayPool componentArrayPool)
         {
             ComponentTypes = componentTypes;
-            ComponentArrayPool = componentArrayPool;
 
             EntityRepo = new EntityRepo(componentTypes, MemoryPool<byte>.Shared, componentArrayPool);
         }
